4d0aa60f0eda7d4704390ec604dccee4d35dc238
Nikita Lysenkov <116703756+HokeyHero@users.noreply.github.com>
2023-03-01 14:37:45 -0500

    Basic functional fully created (#1)
    * Add reference to .NET client
    
    * Create bot client base
    
    * Add start command
    
    * Add dictionary-api parser
    
    * Basic functional fully released

--- README.md
+++ README.md
@@ -2,12 +2,14 @@
 
 ## Description
 
-**Hauturu** — open-source [Telegram bot](https://t.me/HauturuBot) for finding definitions and learning new words.  
+**Hauturu** — open-source [Telegram-bot](https://t.me/HauturuBot) for finding definitions and learning new words.  
 Find words by using presented dictionaries and word lists, or create your own.  
 Practice with exercises for learning new words and remember already learned.
 
 Name of the project inspired by **[kakapo](https://en.wikipedia.org/wiki/K%C4%81k%C4%81p%C5%8D)** — one of the oldest bird on the earth.  
 Kakapo is _endangered species_ today. Arrival of humans was _main factor in the decline of the kakapo_.
+
+The project is based on [TelegramBot client](https://github.com/TelegramBots/Telegram.Bot) and uses [Free Dictionary API](https://github.com/meetDeveloper/freeDictionaryAPI).
 
 ## License
 
---
+++ Source/Hauturu/DictionaryClient.cs
@@ -0,0 +1,84 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Hauturu
+{
+    internal class DictionaryClient
+    {
+        public async Task<WordData?> GetDefinitionAsync(string word)
+        {
+            var response = await GetResponseAsync($"https://api.dictionaryapi.dev/api/v2/entries/en/{word}");
+            List<WordData>? definition;
+
+            try
+            {
+                definition = JsonSerializer.Deserialize<List<WordData>>(response);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+
+            return definition[0];
+        }
+
+        async Task<string> GetResponseAsync(string url)
+        {
+            HttpClient httpClient = new();
+            var request = new HttpRequestMessage(HttpMethod.Get, url);
+            var response = await httpClient.SendAsync(request);
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        internal class WordData
+        {
+            [JsonPropertyName("word")]
+            public string Word { get; set; }
+
+            [JsonPropertyName("phonetic")]
+            public string? Phonetic { get; set; }
+
+            [JsonPropertyName("phonetics")]
+            public List<PhoneticArray>? Phonetics { get; set; }
+
+            internal class PhoneticArray
+            {
+                [JsonPropertyName("text")]
+                public string? Text { get; set; }
+
+                [JsonPropertyName("audio")]
+                public string? Audio { get; set; }
+            }
+
+            [JsonPropertyName("origin")]
+            public string? Origin { get; set; }
+
+            [JsonPropertyName("meanings")]
+            public List<MeaningArray> Meanings { get; set; }
+            
+            internal class MeaningArray
+            {
+                [JsonPropertyName("partOfSpeech")]
+                public string PartOfSpeech { get; set; }
+
+                [JsonPropertyName("definitions")]
+                public List<DefinitionArray> Definitions { get; set; }
+
+                internal class DefinitionArray
+                {
+                    [JsonPropertyName("definition")]
+                    public string Definition { get; set; }
+
+                    [JsonPropertyName("example")]
+                    public string? Example { get; set; }
+
+                    [JsonPropertyName("synonyms")]
+                    public string[]? Synonyms { get; set; }
+
+                    [JsonPropertyName("antonyms")]
+                    public string[]? Antonyms { get; set; }
+                }
+            }
+        }
+    }
+}
